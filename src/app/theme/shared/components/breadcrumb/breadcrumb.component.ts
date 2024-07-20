fc6c1938615598392a474a7923b6a32e21584938
Familia Berson <bersons@Familias-MacBook-Air.local>
2024-07-20 19:25:04 -0300

    init commit

--- src/app/theme/shared/components/breadcrumb/breadcrumb.component.ts
+++ src/app/theme/shared/components/breadcrumb/breadcrumb.component.ts
@@ -46,7 +46,7 @@ export class BreadcrumbComponent {
         const breadcrumbList = this.filterNavigation(this.navigations, activeLink);
         this.navigationList = breadcrumbList;
         const title = breadcrumbList[breadcrumbList.length - 1]?.title || 'Welcome';
-        this.titleService.setTitle(title + ' | Ide e pregai o evangelho');
+        this.titleService.setTitle(title);
       }
     });
   }
